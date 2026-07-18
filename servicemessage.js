82d80fb8551688fad11d6b784d84ea83637da63c
jijoy-jk-twork-ux <jijoytwork@gmail.com>
2026-07-18 23:22:41 +0530

    Update servicemessage.js

--- servicemessage.js
+++ servicemessage.js
@@ -18,7 +18,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/infinity.png' // ആപ്പിന്റെ ലോഗോ
+    icon: '/Infinity.png' // ആപ്പിന്റെ ലോഗോ
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
